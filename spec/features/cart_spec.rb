7495dd1d411daefebaba63af48d73696a4b9e260
Andrew Watkins <andrew.watkinz@gmail.com>
2014-09-18 14:15:23 -0600

    can remove contributions from cart

--- spec/features/cart_spec.rb
+++ spec/features/cart_spec.rb
@@ -12,6 +12,7 @@ describe 'Shopping Contributions', type: :feature do
     first(:button, 'Loan Now').click
     total_contribution = find('span.badge').text
     expect(total_contribution).to eq '1'
+    expect(page).to have_content "You've added a $25.00 contribution to your cart"
   end
 
   xit 'cannot add the same contribution multiple times' do
@@ -35,6 +36,15 @@ describe 'Shopping Contributions', type: :feature do
     click_link 'View Contributions'
     expect(page).to have_content @loan.title
     expect(page).to have_content "$25.00"
+  end
+
+  it 'can delete contributions from the cart' do
+    visit loans_path
+    first(:button, 'Loan Now').click
+    click_link 'View Contributions'
+    click_on 'Remove'
+    expect(page).to_not have_content @loan.title
+    expect(page).to have_content "Your cart is empty."
   end
 
   xit 'can change the contribution amount' do

e4c6b096f26eda803117f9cbd6f5fb3bcaac3027
Andrew Watkins <andrew.watkinz@gmail.com>
2014-09-18 12:15:03 -0600

    categories spec girlified

--- spec/features/categories_spec.rb
+++ spec/features/categories_spec.rb
@@ -1,29 +1,14 @@
 require 'rails_helper'
 
-xdescribe 'when viewing the categories' do
+describe 'when viewing the categories' do
 	context 'as a guest' do
 
-		let(:category) { Category.create(id: 1,
-																		name: 'Savory'
-																		) }
-		let(:loan) { Loan.create(id: 1,
-														title: 'Buy a cow',
-														description: 'Need to buy a milking cow for our farm',
-														amount: 50000,
-														requested_by: "2014-09-10 13:43:00 -0600",
-														repayments_begin: "2014-09-10 13:43:00 -0600",
-														monthly_payment: 1000,
-														user_id: 1
-														) }
-		let(:loan_category) { LoanCategory.create(loan_id: 1, category_id: 1) }
-
 		before(:each) do
-			category
-			loan_category
-			loan
+			@category = create(:category)
+			@loan = create(:loan)
+			@category.loans << @loan
 			visit loans_path
 		end
-
 
 		it 'cannnot edit, delete, or add categories' do
 			expect(page).not_to have_content('Edit')
@@ -32,9 +17,9 @@ xdescribe 'when viewing the categories' do
 		end
 
 		it "can see loans by categories" do
-			expect(page).to have_content(category.name)
-			expect(page).to have_content(loan.title)
-			expect(page).to have_content(loan.amount_in_dollars)
+			expect(page).to have_content(@category.name)
+			expect(page).to have_content(@loan.title)
+			expect(page).to have_content(@loan.amount_in_dollars)
 		end
 	end
 end

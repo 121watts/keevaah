ca13677d713b2c519176a9b1ac6e3ed04e20145e
Hilary Denton <Hilarydenton90@gmail.com>
2014-09-18 14:05:59 -0600

    newest category stuff

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -14,7 +14,9 @@ class LoansController < ApplicationController
 	  end
 
 		# group by category
+		set_newest_loans
 		@loans_by_category = {}
+
 		loan_requests.each do |loan|
 			loan.categories.each do |category|
 				@loans_by_category[category.name] ||= []
@@ -31,5 +33,14 @@ class LoansController < ApplicationController
 
 	def loan_requests
 		Loan.request.decorate
+	end
+
+	def set_newest_loans
+		binding.pry
+		newest_category = Category.find_by(name: "Newest")
+		newest_loans = Loan.order(:created_at).limit(5)
+		newest_loans.map do |loan|
+			LoanCategory.create(loan_id: loan.id, category_id: newest_category.id)
+		end
 	end
 end

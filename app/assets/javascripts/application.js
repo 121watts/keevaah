20dcaa2d7d130f372886ea31aeb90ab76e22ef05
Andrew Watkins <andrew.watkinz@gmail.com>
2014-09-24 10:24:14 -0600

    borrower spec passing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require twitter/bootstrap
-//= require bootstrap
-//= require jquery.turbolinks
-//= require turbolinks
-//= require masonry/jquery.masonry
-//= require_tree .
-//= require bootstrap-sprockets
+// = require jquery
+// = require jquery_ujs
+// = require twitter/bootstrap
+// = require bootstrap
+// = require jquery.turbolinks
+// = require turbolinks
+// = require masonry/jquery.masonry
+// = require_tree .
+// = require bootstrap-sprockets
